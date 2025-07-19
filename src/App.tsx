722f63fba8b960d53122095fe485c026f46d33ee
pothamsetty <pothamsetty17@gmail.com>
2025-07-19 11:20:49 +0530

    resolved build errors

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,8 @@
-import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import Layout from './components/common/Layout';
 import HomePage from './pages/HomePage';
 import AboutPage from './pages/AboutPage';
-import ToolsPage from './pages/ToolsPage';
-import TokenomicsPage from './pages/TokenomicsPage';
-import RoadmapPage from './pages/RoadmapPage';
+
 import ContactPage from './pages/ContactPage';
 
 function App() {
--- src/pages/ToolsPage.tsx
+++ src/pages/ToolsPage.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { motion } from 'framer-motion';
-import ToolCard from '../components/ui/ToolCard';
+
 
 const ToolsPage: React.FC = () => {
   const tools = [
